78b6424e3cdbc2fa4e810251bff4684d5c05fd4a
Jose Carlo Navarrete Carrillo <jose.navarrete@gobierno1.yucatan.gob.mx>
2021-09-21 12:16:27 -0500

    updated

---
+++ R/Create_parquet.md
@@ -0,0 +1,15 @@
+---
+title: "Transforming csv data to parquet format"
+author: "Jose Carlo Navarrete"
+date: "19/9/2021"
+output: html_document
+editor_options: 
+  chunk_output_type: console
+---
+
+Setting up libraries
+
+
+Reading up to date COVID-19 csv file
+
+
--- R/updateR.R
+++ R/updateR.R
@@ -1,7 +1,7 @@
 library(git2r)
 
 # Renders the html to update
-rmarkdown::render(input = "Casos-vacunados-raros.Rmd", output_file = "second.html")
+rmarkdown::render(input = "Casos-vacunados-raros.Rmd", output_file = "../html/index.html")
 
 # Adds all the files for a new commit
 add(repo = "./", path = ".")
