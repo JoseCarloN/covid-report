45c8eb926302c13d06d15b2a034c332b5c1a88b1
ghostdoggie1 <josephnavarrete2@gmail.com>
2021-09-22 10:11:56 -0500

    updated

--- R/updateR.R
+++ R/updateR.R
@@ -1,16 +1,36 @@
 library(git2r)
 
+# Adjust the date of the report acordingly 
+fecha_reporte = Sys.Date() - 1
+
 # Renders the html to update
-rmarkdown::render(input = "Casos-vacunados-raros.Rmd", output_file = "../html/index.html")
+rmarkdown::render(input = "Casos-vacunados-raros.Rmd", params = list(
+    report_date = fecha_reporte
+  ), 
+  output_file = "../html/index.html")
+
+
+# Login credentials
+credentials = cred_ssh_key(
+  publickey = "../secrets/key.pub",
+  privatekey = "../secrets/key"
+)
+
+
+# Repo config
+config(repo = "../html", global = TRUE, user.name = "ghostdoggie1", user.email = "josephnavarrete2@gmail.com")
+
+# Checks the status of the repo before adding files
+status(repo = "../html")
 
 # Adds all the files for a new commit
-add(repo = "./", path = ".")
+add(repo = "../html", path = ".")
 
-# Checks the status of the repo
-status("./")
+# Checks the status of the repo after adding files
+status(repo = "../html")
 
 # Commits
-commit(repo = "./", message = "updated to most recent")
+commit(repo = "../html", message = "updated to most recent")
 
 # Push the commit
-push()
+push(object = "../html", name = "origin", refspec = "main", credentials = credentials)
